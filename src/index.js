40b86253cf6aa11afc6393f2167edfa2d175b0fe
anapaulalaraujo <aplaraujo@outlook.com>
2019-10-12 18:14:05 -0300

    Modificações finais no projeto

--- src/index.js
+++ src/index.js
@@ -64,3 +64,49 @@ const decode = (string, offset) => {
     }
     return result
 }
+
+const enterButton = document.querySelector('.myButton');
+enterButton.addEventListener('click', () => {
+    TweenMax.to('.myButton', 1, {
+        y: -100,
+        opacity: 0
+    });
+
+    TweenMax.to('.screen', 2, {
+        y: -400,
+        opacity: 0,
+        ease: Power2.easeInOut,
+        delay: 2
+    });
+
+    TweenMax.from('.overlayOne', 2, {
+        ease: Power2.easeInOut
+    });
+
+    TweenMax.to('.overlayOne', 2, {
+        delay: 2.6,
+        top: '-110%',
+        ease: Expo.easeInOut 
+    });
+
+    TweenMax.to('.overlayTwo', 2, {
+        delay: 3,
+        top: '-110%',
+        ease: Expo.easeInOut 
+    });
+
+    TweenMax.from('.content', 2, {
+        delay: 3.2,
+        opacity: 0,
+        ease: Power2.easeInOut
+    });
+
+    TweenMax.to('.content', 2, {
+        opacity: 1,
+        y: -300,
+        delay: 3.2,
+        ease: Power2.easeInOut
+    });
+
+})
+
