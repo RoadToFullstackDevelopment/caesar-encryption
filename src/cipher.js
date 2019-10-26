fc7169389e859c6967708aa0930a990232964ea0
anapaulalaraujo <aplaraujo@outlook.com>
2019-10-26 11:07:22 -0300

    Modificação das funções de codificação e decodificação

--- src/cipher.js
+++ src/cipher.js
@@ -1,47 +1,23 @@
 const encode = (string, offset) => {
     let result = ''
+    offset = offset % 94
     for (let i = 0; i < string.length; i++) {
-        let encodedCharacter = string.charCodeAt(i)
-        if (encodedCharacter >= 33 & encodedCharacter <= 64) {
-        result += String.fromCharCode(((encodedCharacter - 32 + offset) % 33) + 32)
-        }
-        else if (encodedCharacter >= 65 & encodedCharacter <= 90) {
-        result += String.fromCharCode(((encodedCharacter - 65 + offset) % 26) + 65)
-        }
-        else if (encodedCharacter >= 97 & encodedCharacter <= 122) {
-        result += String.fromCharCode(((encodedCharacter - 97 + offset) % 26) + 97)
-        }
-        else if (encodedCharacter >= 192 & encodedCharacter <= 255) {
-        result += String.fromCharCode(((encodedCharacter - 192 + offset) % 64) + 192)
-        }
-        else {
-        result += string.charAt(i)
-        }
-    }
-return result
-}
-
-const decode = (string, offset) => {
-    let result = ''
-    for (let i = 0; i < string.length; i++) {
-        let encodedCharacter = string.charCodeAt(i)
-        if (encodedCharacter >= 33 & encodedCharacter <= 64) {
-            result += String.fromCharCode(((encodedCharacter - 64 - offset) % 33) + 64)
-        }
-        else if (encodedCharacter >= 65 & encodedCharacter <= 90) {
-            result += String.fromCharCode(((encodedCharacter - 90 - offset) % 26) + 90)
-        }
-        else if (encodedCharacter >= 97 & encodedCharacter <= 122) {
-            result += String.fromCharCode(((encodedCharacter - 122 - offset) % 26) + 122)
-        }
-        else if (encodedCharacter >= 192 & encodedCharacter <= 255) {
-            result += String.fromCharCode(((encodedCharacter - 255 - offset) % 64) + 255)
+        let encodeCharacter = string.charCodeAt(i)
+        if (encodeCharacter >= 32 & encodeCharacter <= 126) {
+        result += String.fromCharCode(((encodeCharacter - 32 + offset) % 94) + 32)
         }
         else {
             result += string.charAt(i)
         }
     }
     return result
+};
+
+const decode = (string, offset) => {
+    let result = '';
+    offset = (94 - offset) % 94;
+    result = encode(string, offset);
+    return result;
 }
 
 export { encode, decode }
