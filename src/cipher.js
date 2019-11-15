a294c3008120fca37e2e9bf69ec0cd7540bc3588
anapaulalaraujo <aplaraujo@outlook.com>
2019-11-15 14:45:30 -0300

    Tranformação das funções de codificação e decodificação em uma única função

--- src/cipher.js
+++ src/cipher.js
@@ -1,23 +1,7 @@
 const encode = (string, offset) => {
-    let result = ''
-    offset = offset % 94
-    for (let i = 0; i < string.length; i++) {
-        let encodeCharacter = string.charCodeAt(i)
-        if (encodeCharacter >= 32 & encodeCharacter <= 126) {
-        result += String.fromCharCode(((encodeCharacter - 32 + offset) % 94) + 32)
-        }
-        else {
-            result += string.charAt(i)
-        }
-    }
-    return result
+    return String.fromCharCode(
+        ...string.split('').map(char => ((char.charCodeAt() - 32 + (95 + offset % 95)) % 95) + 32),
+    );
 };
 
-const decode = (string, offset) => {
-    let result = '';
-    offset = (94 - offset) % 94;
-    result = encode(string, offset);
-    return result;
-}
-
-export { encode, decode };
+export { encode };
